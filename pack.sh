4a3ada9d2ff992bcb7ee2556697d496b8ac85d65
GBLodb <garybluis@outlook.com>
2021-03-26 20:30:47 +0800

    init

---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+ZIP_FILE_NAME="Pheidias UI Pack-build${GITHUB_RUN_NUMBER}.zip"
+
+[ -d "${GITHUB_WORKSPACE}" ] && PROJECT_DIR="${GITHUB_WORKSPACE}"
+[ "${PROJECT_DIR}" != "${PWD}" ] && cd "${PROJECT_DIR}"
+
+zip -r "${ZIP_FILE_NAME}" -xi assets icon.png pack.mcmeta
+mv {,artifacts/}${ZIP_FILE_NAME}
